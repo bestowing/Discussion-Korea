a6fdfaa419a1892eebfd1b6b1488eead65319043
청수 <bestowing@naver.com>
2022-03-17 21:35:04 +0900

    feat: 입력 폼 및 send 버튼 추가, 버튼 선택가능 변경 구현

--- Discussion-Korea/Discussion-Korea/ChatRoomScene/ChatRoomViewController.swift
+++ Discussion-Korea/Discussion-Korea/ChatRoomScene/ChatRoomViewController.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 class ChatRoomViewController: UIViewController {
+    @IBOutlet private weak var messageTextView: UITextView!
+    @IBOutlet private weak var sendButton: UIButton!
 
     // MARK: methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sendButton.isEnabled = !self.messageTextView.text.isEmpty
+        self.messageTextView.delegate = self
+    }
+
+    @IBAction func sendButtonDidTouch(_ sender: UIButton) {
+        // TODO: send 버튼 터치 구현
+    }
+
+}
+
+extension ChatRoomViewController: UITextViewDelegate {
+
+    func textViewDidChange(_ textView: UITextView) {
+        self.sendButton.isEnabled = !textView.text.isEmpty
     }
 
 }
