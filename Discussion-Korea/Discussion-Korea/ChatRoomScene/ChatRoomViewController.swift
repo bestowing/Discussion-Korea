6b83dbd5f0845d698fe69625d70cef0f98835243
청수 <bestowing@naver.com>
2022-03-18 21:08:34 +0900

    feat: 콜렉션뷰 추가

--- Discussion-Korea/Discussion-Korea/ChatRoomScene/ChatRoomViewController.swift
+++ Discussion-Korea/Discussion-Korea/ChatRoomScene/ChatRoomViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ChatRoomViewController: UIViewController {
+    @IBOutlet private weak var messageCollectionView: UICollectionView!
     @IBOutlet private weak var messageTextView: UITextView!
     @IBOutlet private weak var sendButton: UIButton!
 
@@ -16,11 +17,28 @@ class ChatRoomViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sendButton.isEnabled = !self.messageTextView.text.isEmpty
+        self.messageCollectionView.delegate = self
+        self.messageCollectionView.dataSource = self
         self.messageTextView.delegate = self
     }
 
     @IBAction func sendButtonDidTouch(_ sender: UIButton) {
         // TODO: send 버튼 터치 구현
+    }
+
+}
+
+extension ChatRoomViewController: UICollectionViewDelegate,
+                                  UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // TODO: 구현 필요
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // TODO: 구현 필요
+        return UICollectionViewCell()
     }
 
 }
