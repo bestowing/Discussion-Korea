1b0804fe2cc139dfae43ab5c13ee9a3a579c8b02
청수 <bestowing@naver.com>
2022-03-21 01:44:04 +0900

    feat: 사용자정보 구조체 추가

---
+++ Discussion-Korea/Discussion-Korea/ChatRoomScene/UserInfo.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfo.swift
+//  Discussion-Korea
+//
+//  Created by 이청수 on 2022/03/21.
+//
+
+import Foundation
+
+struct UserInfo {
+
+    var userID: String
+    var nickname: String
+    var profileURL: URL?
+
+}
