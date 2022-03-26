2b58c79bd2f8db1b6dec7d7dcb2a485e603f0d09
청수 <bestowing@naver.com>
2022-03-27 02:28:20 +0900

    feat: 추가 정보 사이드메뉴 구현

---
+++ Discussion-Korea/Discussion-Korea/ChatRoomDetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ChatRoomDetailViewController.swift
+//  Discussion-Korea
+//
+//  Created by 이청수 on 2022/03/27.
+//
+
+import UIKit
+
+final class ChatRoomDetailViewController: UIViewController {
+
+}
