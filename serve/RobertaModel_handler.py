ca5b7b17b30ed1c2e31fbbc42cb084cd21fcac9f
seokcoding <seokcoding@gmail.com>
2022-04-03 12:41:00 +0900

    make RobertaModel_handler.py

--- serve/RobertaModel_handler.py
+++ serve/RobertaModel_handler.py
@@ -2,12 +2,13 @@ from abc import ABC
 import json
 import logging
 import os
-import ast
 import torch
 import transformers
-from transformers import AutoTokenizer
-from base_handler import BaseHandler
-from .model import RobertaModel
+from transformers import AutoTokenizer, AutoModel, AutoConfig
+from ts.torch_handler.base_handler import BaseHandler
+import torch.nn as nn
+import importlib.util
+import inspect
 
 logger = logging.getLogger(__name__)
 logger.info("Transformers version %s",transformers.__version__)
@@ -33,25 +34,46 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
         model_dir = properties.get("model_dir")
         serialized_file = self.manifest["model"]["serializedFile"]
         model_pt_path = os.path.join(model_dir, serialized_file)
+        model_file = self.manifest["model"].get("modelFile", "")
+        model_path = os.path.join(model_dir, model_file)
+        self.tokenizer = AutoTokenizer.from_pretrained("klue/roberta-base")
 
         self.device = torch.device(
             "cuda:" + str(properties.get("gpu_id"))
             if torch.cuda.is_available() and properties.get("gpu_id") is not None
             else "cpu"
         )
+
+        # Loading the model and tokenizer from checkpoint and config files based on the user's choice of mode
+        # further setup config can be added.
         
-        self.ckpt = "./basemodel.pt"
-        self.tokenizer = AutoTokenizer.from_pretrained("klue/roberta-base")
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.model = RobertaModel("klue/roberta-base", 2)
+        # self.model = AutoModel.from_pretrained("klue/roberta-base", 2)
+        # self.model = RobertaModel("klue/roberta-base", 2)
         
-        self.model.load_state_dict(torch.load(self.ckpt, map_location=torch.device('cpu'))['model_state_dict'])
-        # self.model.to(self.device)
         
+        # self.model.load_state_dict(torch.load(model_pt_path, map_location=torch.device('cpu')))
+        # self.model = torch.jit.load(model_pt_path, map_location=self.device) # bin file
+
+        module = importlib.import_module(model_file.split(".")[0])
+        model_class_definitions = [cls[1] for cls in inspect.getmembers(module, lambda member: inspect.isclass(member) and member.__module__ == module.__name__)]
+        
+        if len(model_class_definitions) != 1:
+            raise ValueError(
+                "Expected only one class as model definition. {}".format(
+                    model_class_definitions
+                )
+            )
+        model_class = model_class_definitions[0]
+        self.model = model_class("klue/roberta-base", 2)
+    
+    
+        self.model.load_state_dict(torch.load(model_pt_path, map_location=torch.device('cpu'))['model_state_dict'])
+        
+        self.model.to(self.device)
         self.model.eval()
 
         logger.info(
-            "klue-roberta-base model from path %s loaded successfully", model_dir
+            "Transformer model from path %s loaded successfully", model_dir
         )
 
         # Read the mapping file, index to object name
@@ -72,10 +94,15 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
         Returns:
             list : The preprocess function returns a list of Tensor for the size of the word tokens.
         """
-        input = torch.tensor(self.tokenizer.encode(requests), device=self.device)
-        return input.unsqueeze(0)
+        input_name = requests[0]["body"]["name"]
+        input_text = requests[0]["body"]["text"]
+        
+        input = torch.tensor(self.tokenizer.encode(input_text), device=self.device)
+        print(input)
+        
+        return (input.unsqueeze(0), input_name)
 
-    def inference(self, input_text):
+    def inference(self, input):
         """Predict the class (or classes) of the received text using the
         serialized transformers checkpoint.
         Args:
@@ -83,13 +110,20 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
         Returns:
             list : It returns a list of the predicted value for the input text
         """
-        predictions = self.model(input_text)
-        prediction = torch.argmax(predictions, dim=-1)
+        name = input[1]
+        input_text = input[0]
+        output = self.model(input_text)
+        try:
+            logits, _ = output
+        except:
+            logits = output
+            
+        prediction = torch.argmax(logits, dim=-1)
 
         if int(prediction[0]) == 0:
-            return 0
+            return [name + "님 " + "말을 이쁘게 하시네요"]
         else:
-            return 1
+            return [name + "님 " + "말을 나쁘게 하시네요"]
 
     def postprocess(self, inference_output):
         """Post Process Function converts the predicted response into Torchserve readable format.
@@ -98,4 +132,5 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
         Returns:
             (list): Returns a list of the Predictions and Explanations.
         """
+        
         return inference_output
--- serve/model.py
+++ serve/model.py
@@ -25,23 +25,4 @@ class RobertaModel(nn.Module):
             criterion = nn.CrossEntropyLoss()
             loss = criterion(logits.view(-1,self.num_labels), labels.view(-1))
             
-        return logits
-    
-class StudentModel(nn.Module):
-    """
-    Student Model for classification
-    """
-    def __init__(self, num_labels):
-        super(StudentModel, self).__init__()
-        self.num_labels = num_labels
-        
-        self.model = model = DistilBertModel.from_pretrained('monologg/distilkobert')
-        self.dropout = nn.Dropout(0.1)
-        self.classifier = nn.Linear(768, num_labels)
-        
-    def forward(self, input_ids=None, attention_maks=None):
-        outputs = self.model(input_ids=input_ids, attention_mask=attention_maks)
-        outputs = self.dropout(outputs[0])
-        logits = self.classifier(outputs[:,0,:].view(-1,768))
-
-        return logits
+        return logits, loss
