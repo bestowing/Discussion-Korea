a30b545573af65337eb557365c5e3f1fab64b39b
청수 <bestowing@naver.com>
2022-04-29 18:31:23 +0900

    feat: 네트워크 플랫폼 추가

---
+++ NetworkPlatform/NetworkPlatform/NetworkPlatform.h
@@ -0,0 +1,18 @@
+//
+//  NetworkPlatform.h
+//  NetworkPlatform
+//
+//  Created by 이청수 on 2022/04/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkPlatform.
+FOUNDATION_EXPORT double NetworkPlatformVersionNumber;
+
+//! Project version string for NetworkPlatform.
+FOUNDATION_EXPORT const unsigned char NetworkPlatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkPlatform/PublicHeader.h>
+
+
