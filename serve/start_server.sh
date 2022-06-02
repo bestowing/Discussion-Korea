be68d9225c0562e43a57bd1940670e9abb2f27e9
seokcoding <seokcoding@gmail.com>
2022-06-03 00:56:32 +0900

    update script files

--- serve/start_server.sh
+++ serve/start_server.sh
@@ -1,3 +1,1 @@
-# torchserve --start --model-store model_store --models classification=klue-roberta-base-v1.0.mar summarization=kobart-v1.0.mar --ncs
-
-torchserve --start --model-store model_store --models classification1=multi-label-klue-roberta-base-v1.0.mar classification2=klue-roberta-base-v1.1.mar --ncs
+torchserve --start --model-store model_store --models classification1=klue-roberta-base-v1.1.mar classification3=multi-label-klue-roberta-base-v1.0.mar summarization=kobart-v1.3.mar --ncs
