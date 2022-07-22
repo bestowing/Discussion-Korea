589da1f618550edcdc24b867a98d774973cd93f9
청수 <bestowing@naver.com>
2022-07-23 00:25:38 +0900

    feat: 여론조사 뷰 비율 적용

--- Discussion-Korea/Presentation/Scenes/ChatRoomSideMenu/View/OpinionView.swift
+++ Discussion-Korea/Presentation/Scenes/ChatRoomSideMenu/View/OpinionView.swift
@@ -21,7 +21,34 @@ final class OpinionView: UIView {
         return label
     }()
 
-    fileprivate var sideControl: UISegmentedControl = {
+    fileprivate let chartView: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+
+    fileprivate let agreeView: UILabel = {
+        let label = PaddingLabel(padding: UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0))
+        label.text = "0"
+        label.font = UIFont.systemFont(ofSize: 13.0)
+        label.textAlignment = .left
+        label.layer.cornerRadius = 10.0
+        label.layer.masksToBounds = true
+        label.backgroundColor = UIColor.agreeColor
+        return label
+    }()
+
+    fileprivate let disagreeView: UILabel = {
+        let label = PaddingLabel(padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10))
+        label.text = "0"
+        label.font = UIFont.systemFont(ofSize: 13.0)
+        label.textAlignment = .right
+        label.layer.cornerRadius = 10.0
+        label.layer.masksToBounds = true
+        label.backgroundColor = UIColor.disagreeColor
+        return label
+    }()
+
+    fileprivate let sideControl: UISegmentedControl = {
         let control = UISegmentedControl(items: [
             "찬성", "반대", "기타"
         ])
@@ -42,13 +69,33 @@ final class OpinionView: UIView {
 
     private func setSubviews() {
         self.addSubview(self.supportLabel)
+        self.addSubview(self.chartView)
+        self.addSubview(self.agreeView)
+        self.addSubview(self.disagreeView)
         self.addSubview(self.sideControl)
         self.supportLabel.snp.makeConstraints { make in
             make.leading.trailing.top.equalToSuperview()
         }
+        self.chartView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+        }
+        self.agreeView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalTo(self.supportLabel.snp.bottom).offset(10)
+            make.width.equalTo(self.chartView.snp.width).multipliedBy(0.5).priority(.medium).constraint
+            make.width.greaterThanOrEqualTo(30).priority(.high)
+            make.height.equalTo(50)
+        }
+        self.disagreeView.snp.makeConstraints { make in
+            make.leading.equalTo(self.agreeView.snp.trailing).priority(.medium)
+            make.trailing.equalToSuperview()
+            make.top.equalTo(self.agreeView.snp.top)
+            make.width.greaterThanOrEqualTo(30).priority(.high)
+            make.height.equalTo(self.agreeView.snp.height)
+        }
         self.sideControl.snp.makeConstraints { make in
             make.leading.trailing.bottom.equalToSuperview()
-            make.top.equalTo(self.supportLabel.snp.bottom).offset(10)
+            make.top.equalTo(self.agreeView.snp.bottom).offset(10)
         }
     }
 
@@ -60,6 +107,18 @@ extension Reactive where Base: OpinionView {
         return base.sideControl.rx.value
     }
 
+    var ratio: Binder<Double> {
+        return Binder(self.base) { opinionView, ratio in
+            opinionView.agreeView.snp.remakeConstraints { make in
+                make.leading.equalToSuperview()
+                make.top.equalTo(opinionView.supportLabel.snp.bottom).offset(10)
+                make.width.equalTo(opinionView.chartView.snp.width).multipliedBy(ratio).priority(.medium)
+                make.width.greaterThanOrEqualTo(30).priority(.high)
+                make.height.equalTo(50)
+            }
+        }
+    }
+
     var side: Binder<Side?> {
         return Binder(self.base) { opinionView, side in
             guard let side = side else { return }
@@ -67,7 +126,6 @@ extension Reactive where Base: OpinionView {
             guard let index = indexes.firstIndex(of: side)
             else { return }
             opinionView.sideControl.selectedSegmentIndex = index
-            
         }
     }
 
