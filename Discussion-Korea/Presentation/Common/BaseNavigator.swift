28d19c1d055563deb3fdd3810f410dbaa5973626
청수 <bestowing@naver.com>
2022-08-31 04:20:51 +0900

    refactor: BaseNavigator 추가
    편의성을 위함

---
+++ Discussion-Korea/Presentation/Common/BaseNavigator.swift
@@ -0,0 +1,15 @@
+//
+//  BaseNavigator.swift
+//  Discussion-Korea
+//
+//  Created by 이청수 on 2022/08/25.
+//
+
+/// Navigator에서 반복되는 코드를 줄이기 위한 클래스
+class BaseNavigator {
+
+    deinit {
+        print("🗑", self)
+    }
+
+}
