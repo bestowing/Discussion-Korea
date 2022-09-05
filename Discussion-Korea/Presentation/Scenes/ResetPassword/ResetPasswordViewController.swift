a6f994a2aecff801725e5b2ce834072951abca7a
청수 <bestowing@naver.com>
2022-09-06 05:29:20 +0900

    feat: 재설정 화면 뷰 추가

--- Discussion-Korea/Presentation/Scenes/ResetPassword/ResetPasswordViewController.swift
+++ Discussion-Korea/Presentation/Scenes/ResetPassword/ResetPasswordViewController.swift
@@ -22,18 +22,63 @@ final class ResetPasswordViewController: BaseViewController {
         return button
     }()
 
+    private let idField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "이메일"
+        textField.keyboardType = .emailAddress
+        textField.font = .preferredFont(forTextStyle: .body)
+        textField.adjustsFontForContentSizeCategory = true
+        return textField
+    }()
+
+    private let resetButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("재설정 메일 보내기", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.backgroundColor = UIColor.accentColor
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
+        return button
+    }()
+
     private let disposeBag = DisposeBag()
 
     // MARK: - methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "비밀번호 재설정"
         self.setSubViews()
         self.bindViewModel()
     }
 
     private func setSubViews() {
+        let descriptionLabel: UILabel = {
+            let label = ResizableLabel()
+            label.text = "비밀번호를 재설정할 수 있는 메일을 보내드려요"
+            label.font = UIFont.preferredFont(forTextStyle: .caption1)
+            label.textColor = .secondaryLabel
+            return label
+        }()
+
         self.navigationItem.leftBarButtonItem = self.exitButton
+        self.view.addSubview(self.idField)
+        self.view.addSubview(self.resetButton)
+        self.view.addSubview(descriptionLabel)
+
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(15)
+            make.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(30)
+        }
+        self.idField.snp.makeConstraints { make in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(20)
+            make.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(30)
+        }
+        self.resetButton.snp.makeConstraints { make in
+            make.top.equalTo(self.idField.snp.bottom).offset(10)
+            make.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(30)
+        }
     }
 
     private func bindViewModel() {
